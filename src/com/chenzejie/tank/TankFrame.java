1cc029a330840f4fd8a982f96842cdab3e9fbcc4
chenzejie <2752713798@qq.com>
2021-07-20 23:46:13 +0800

    将坦克坐标定为变量，在paint方法中改变值，paint方法被调用后，就可以动起来

--- src/com/chenzejie/tank/TankFrame.java
+++ src/com/chenzejie/tank/TankFrame.java
@@ -5,6 +5,9 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+    int x = 200;
+    int y = 200;
+
     public TankFrame() throws HeadlessException {
         setSize(800,600); // 设置窗口f的大小
         setResizable(false); // 设置窗口f不能改变大小
@@ -21,7 +24,11 @@ public class TankFrame extends Frame {
     // 系统会自动调用这个paint方法
     @Override
     public void paint(Graphics g) {
+        System.out.println("paint");
         // g为一只画笔
-        g.fillRect(200,200,50,50);
+        g.fillRect(x,y,50,50); // 画一个黑方块
+        // 让黑方块动起来
+        x += 10;
+        y += 10;
     }
 }
